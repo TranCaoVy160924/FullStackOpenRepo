b2f4f472830726c7c3ee2c151b372be1df807d51
TranCaoVy160924 <vytcse160924@fpt.edu.vn>
2022-09-02 17:29:28 +0700

    update part 1 exercises

---
+++ Part 1/courseinfo/src/App.js
@@ -0,0 +1,73 @@
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Footer parts={course.parts} />
+    </div>
+  )
+}
+
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.course}</h1>
+    </>
+  )
+}
+
+const Content = (props) => {
+  let parts = props.parts
+
+  console.log(props)
+  return (
+    <>
+      <Part part={parts[0]} />
+      <Part part={parts[1]} />
+      <Part part={parts[2]} />
+    </>
+  )
+}
+
+const Part = (props) => {
+  const part = props.part
+  console.log(part)
+
+  return (
+    <>
+      <p>
+        {part.name} {part.exercises}
+      </p>
+    </>
+  )
+}
+
+const Footer = (props) => {
+  const parts = props.parts
+
+  return (
+    <>
+      <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+    </>
+  )
+}
+
+export default App
